7d574b3ce65098d8e4b741fd20a2687b052e18dc
Aryan Bhasin <bhasinaryan@gmail.com>
2021-02-13 19:18:49 -0500

    Add sample layer to map

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-require(["esri/request", "esri/Map", "esri/views/MapView"], function (
+require(["esri/request", "esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], function (
     esriRequest,
     Map,
-    MapView
+    MapView,
+    FeatureLayer
   ) {
     var url =
       "https://manor-straits-staging.softkraft.net/api/v1/bounding-boxes/?inBbox=38.41055825094609,-80.51879882812501,41.4509614012039,-69.48852539062501&unitsMin=0&unitsMax=10000&strOccupancyMin=0&strOccupancyMax=100&zoom=7";
@@ -13,9 +14,15 @@ require(["esri/request", "esri/Map", "esri/views/MapView"], function (
       console.log(response);
       var geoJson = response.data;
     });
+
+    const layer = new FeatureLayer({
+      url:
+        "https://services3.arcgis.com/dK56ZPamZjkXZiRb/arcgis/rest/services/sample_africa_vaccine_hack/FeatureServer"
+    });
   
     var map = new Map({
-      basemap: "gray"
+      basemap: "gray",
+      layers: [layer]
     });
   
     var view = new MapView({
